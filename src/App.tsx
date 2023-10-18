89bfcbf666273dd62b19a9841903760590155f07
ilia-puchkov <reras@yandex.ru>
2023-10-18 13:39:32 +0300

    add Page Event Formats draft

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Test from './test'
 import PageIntro from './components/PageIntro/PageIntro'
+import PageEventFormats from './components/PageEventFormats/PageEventFormats'
 
 export function App() {
   return (
     <div>
       <Header />
       <PageIntro />
+      <PageEventFormats />
       <Test />
     </div>
   )
---
+++ src/components/PageEventFormats/PageEventFormats.tsx
@@ -0,0 +1,139 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, Typography, Grid } from '@mui/material'
+
+function PageEventFormats() {
+  return (
+    <section>
+      <Grid container spacing={4} justifyContent="center" columns={3}>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card sx={{ maxWidth: 220 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              sx={{ width: '100px', alignSelf: 'center' }}
+              image={bdImage}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                День рождения
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Звезда ТВ-шоу или вечеринки на яхте, а может главный герой
+                иммерсивного театра? Подарок для именинника, который точно
+                запомнится!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </section>
+  )
+}
+
+export default PageEventFormats
