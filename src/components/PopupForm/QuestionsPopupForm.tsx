294bf6eacacd441d62984bc00bb276f27ec1fc48
Ilia Puchkov <112614894+ilia-puchkov@users.noreply.github.com>
2023-10-31 18:41:11 +0300

    add form test

--- src/components/PopupForm/QuestionsPopupForm.tsx
+++ src/components/PopupForm/QuestionsPopupForm.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { TextField, Typography } from '@mui/material'
 import { typoStyle, inputStyle } from './formStyles'
-import useFormValidation from '../../utils/formValidation'
 
 interface QuestionsPopupFormProps {
   questionNumber: number
@@ -12,21 +11,18 @@ function QuestionsPopupForm({
   questionNumber,
   questionText,
 }: QuestionsPopupFormProps) {
-  const { handleChange, values } = useFormValidation()
   return (
     <>
       <Typography variant="h3" sx={typoStyle}>
         {questionText}
       </Typography>
       <TextField
-        onChange={handleChange}
         variant="standard"
         color="primary"
         sx={inputStyle}
         type="text"
         label="Ваш ответ"
         name={`question${questionNumber}`}
-        value={values.questionNumber || ''}
       ></TextField>
     </>
   )
